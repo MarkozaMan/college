eab1e46a719a77e5d90ddec61ffd473ca24e3351
Markoz <MarkAlfredhw@gmail.com>
2025-12-14 06:32:31 +0200

    material n shit

---
+++ material/algorithms/test.cpp
@@ -0,0 +1,63 @@
+#include <iostream>
+
+using namespace std;
+
+// idea is if a % b != 0 THEN a = b b = a % b etc
+
+int GCD(int n, int m)
+{
+    int a = max(n,m);
+    int b = min(n,m);
+    int r = a% b;
+    while (r>0)
+    {
+        a = b;
+        b = r;
+        r = a%b;
+    }
+
+    return b;
+}
+
+void insertion_sort(int* A, int n)
+{
+    for(int i=1; i<n; i++)
+    {
+        while (A[i] < A[i-1] && i > 0)
+        {
+            swap(A[i], A[i-1]);
+            i--;
+
+        }
+    }
+
+}
+
+void selection_sort(int* A, int n)
+{
+    for(int i = 0; i<n; i++)
+    {
+        int min = i;
+        for(int j = i + 1; j < n; j++)
+        {
+            if(A[j] < A[min]) min = j;
+        }
+        swap(A[i],A[min]);
+    }
+}
+
+int main ()
+{
+    cout << "Started...." << "\n";
+    int A[5]= {5,4,1,7,2};
+
+    selection_sort(&A[0], 5);
+
+
+    for(int i=0; i<5; i++)
+    {
+        cout << A[i] << ", ";
+    }
+
+    return 0;
+}
